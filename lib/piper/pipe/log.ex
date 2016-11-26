874b7ff8ba66d0f46e621e06f818f75dece2c26a
Łukasz Jan Niemier <lukasz@niemier.pl>
2016-11-26 12:42:45 +0100

    docs(Piper.Pipe): write short documentation

--- lib/piper/pipe/log.ex
+++ lib/piper/pipe/log.ex
@@ -1,6 +1,14 @@
 defmodule Piper.Pipe.Log do
   @behaviour Piper
 
+  @moduledoc """
+  Pipe that will log `Piper.Data` passed to the `Piper.call\2`.
+
+  ## Options
+
+  - `level` - debug level, defaults to `:debug`
+  """
+
   require Logger
 
   def init(opts), do: opts
